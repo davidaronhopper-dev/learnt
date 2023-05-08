969bd77b043a964f30492a538cddb108a7674e9f
David Hopper <dhopper@ultralinq.com>
2023-05-08 00:16:07 -0500

    Hop-3: Lists in React

--- React/hopperapp/src/App.js
+++ React/hopperapp/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const bookList = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan White',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 function App() {
+
+  const title = 'My Hacker Story';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
+      <hr />
+
+      {bookList.map(function(book){
+        return (
+          <div key={book.objectID}>
+            <h1><a href={book.url} target='blank'>{book.title} by {book.author}</a></h1>
+          </div>
+        );
+      })}
+
     </div>
   );
 }
